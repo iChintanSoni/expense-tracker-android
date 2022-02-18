62c49faf0fe5463ab468ffac5fbe4e4edbd3593c
Chintan Soni <ichintansoni@gmail.com>
2022-02-19 02:56:39 +0530

    Fixed: https://github.com/iChintanSoni/expense-tracker-android/issues/1

--- domain/src/main/java/dev/chintansoni/domain/usecase/CategoryUseCase.kt
+++ domain/src/main/java/dev/chintansoni/domain/usecase/CategoryUseCase.kt
@@ -14,13 +14,14 @@ class CategoryUseCase(
     @Throws(CategoryNotFoundException::class)
     suspend fun deleteCategory(category: Category) {
         val uncategorized: Category? = categoryRepository.getCategoryByName(UncategorizedCategory)
-        val transactions: List<Transaction> =
-            transactionRepository.getAllTransactionsByCategory(categoryId = category.id)
         uncategorized?.let { it ->
+            val transactions: List<Transaction> =
+                transactionRepository.getAllTransactionsByCategory(categoryId = category.id)
             val updatedTransactions = transactions.map { transaction ->
                 transaction.copy(category = it.id)
             }
             transactionRepository.updateTransactions(updatedTransactions)
+            categoryRepository.deleteCategory(category)
         } ?: throw CategoryNotFoundException(category.name)
     }
 }
